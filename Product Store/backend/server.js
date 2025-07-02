57ca3af72949a26833cebeebaaf782a9c8ff1a9e
itz-XD <mi125g8256@gmail.com>
2025-07-02 23:43:58 +0530

    Building API

---
+++ Product Store/backend/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js";
+
+const app = express();
+dotenv.config();
+app.use(express.json());
+
+app.use("/api/products", productRoutes);
+
+app.listen(5000, () => {
+  connectDB();
+  console.log("Server started at http://localhost:5000");
+});
